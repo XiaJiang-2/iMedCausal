45fb7d3be5a087e71aee1821f026a7ac76c58070
Garrett A Barber <79263753+GAB8202@users.noreply.github.com>
2023-03-12 19:14:35 -0400

    Updated formatting of application.py

--- application.py
+++ application.py
@@ -17,6 +17,7 @@ import GES
 application = Flask(__name__)
 ALLOWED_EXTENSIONS = {'txt', 'csv', 'xlsx', 'dat'}
 
+# Returns if file is one of the allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -34,11 +35,15 @@ def transformData():
     if request.method == 'POST':
          if 'file' not in request.files:
             return redirect("/transformData")
+         
          f = request.files['file']
          delim = request.form['delim']
+
          if f and allowed_file(f.filename):
 
             df = pd.read_csv(f, sep=delim)
+
+            # Converts dataset to all numerical values based on the categorical values
             for col_name in df.columns:
                 if(df[col_name].dtype == 'object'):
                     df[col_name] = df[col_name].astype('category')
@@ -56,21 +61,26 @@ def iRCTHome():
 
     if request.method == 'GET':
         error = ''
+
         if request.args.get('error', None) != None:
             error = request.args.get('error', None)
         
         return render_template("iRCT_Home.html", error=error)
+    
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect("/iRCTHome")
+        
         f = request.files['file']
         data_path = 'datasets/' + secure_filename(f.filename)
         delim = request.form['delim']
         functionNum = request.form['functionNum']
+
         if f and allowed_file(f.filename):
             f.save(data_path)
             uploaded_file = True
             return redirect(url_for('iRCT_Page', filename=data_path, uploaded_file=uploaded_file, delim=delim, functionNum = functionNum))
+        
         else:
             return redirect("/iRCTHome")
         
@@ -78,15 +88,16 @@ def iRCTHome():
 def iRCT_Page():
     if request.method == 'GET':
         file_uploaded = bool(request.args.get('uploaded_file', None))
+
         if file_uploaded == True:
             filename = request.args.get('filename', None)
             delim = request.args.get('delim', None)
-
 
             df = pd.read_csv(filename, sep=delim)
             listOfColumns = df.columns
 
             return render_template("iRCT_Page.html", name=filename, columns=listOfColumns, delim=delim, functionNum=request.args.get('functionNum', None))
+        
         else:
             return redirect('/iRCTHome')
     
@@ -97,22 +108,21 @@ def iRCT_Page():
         
         if functionNum == "4":
             covariateCol = request.form['covariate_column']
+
         else:
             covariateCol = None
         fileName = request.form['fileName']
         delimiter = request.form['delimiter']
-
 
         df = pd.read_csv(fileName, sep=delimiter)
         df.index = range(1, len(df)+1, 1)
 
         if treatmentCol == outcomeCol or treatmentCol == covariateCol or outcomeCol == covariateCol:
             errMsg = 'Two of the columns cannot be the same.'
-            # os.remove(fileName)
             return redirect(url_for('iRCTHome', error=errMsg))
+        
         else:
             myiRCT = iRCT.iRCT(df, treatmentCol, outcomeCol, functionNum, covariateCol)
-            # os.remove(fileName)
             return render_template("iRCT_Output.html", result=str(myiRCT.relationVal), outcome=outcomeCol, treatment=treatmentCol)
         
 
@@ -124,12 +134,15 @@ def causalLearnHome():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect("/CausalLearnHome")
+        
         f = request.files['file']
         data_path = 'datasets/' + secure_filename(f.filename)
+
         if f and allowed_file(f.filename):
             f.save(data_path)
             uploaded_file=True
             return redirect(url_for('CausalLearn_Page', filename=data_path, uploaded_file=uploaded_file))
+        
         else:
             return redirect("/CausalLearnHome")
         
@@ -137,10 +150,11 @@ def causalLearnHome():
 def CausalLearn_Page():
     if request.method == 'GET':
         file_uploaded = bool(request.args.get('filename', None))
+
         if file_uploaded == True:
             filename = request.args.get('filename', None)
-
             return render_template("CausalLearn_Page.html", name=filename)
+        
         else:
             return redirect('/CausalLearnHome')
     
@@ -179,8 +193,8 @@ def PC_Page():
 def rFCI_Page():
     if request.method == 'GET':
         fileName = request.args.get('name', None)
-
         return render_template("rFCI.html", name=fileName)
+    
     if request.method == 'POST':
         fileName = request.form['fileName']
         delimiter = request.form['delim']
@@ -189,14 +203,12 @@ def rFCI_Page():
         f = open("templates/rFCI_Outputs/rFCI_Result.txt", 'r')
         rFCI_Data = f.readlines()
         f.close()
-        # os.remove(fileName)
         return render_template("rFCI_Output.html", Lines=rFCI_Data)
 
 @application.route("/FGES", methods=['GET', 'POST'])
 def FGES_Page():
     if request.method == 'GET':
         fileName = request.args.get('name', None)
-
         return render_template("FGES.html", name=fileName)
 
     if request.method == 'POST':
@@ -207,42 +219,38 @@ def FGES_Page():
         f = open("templates/FGES_Outputs/FGES_Result.txt", 'r')
         FGES_Data = f.readlines()
         f.close()
-        # os.remove(fileName)
         return render_template("FGES_Output.html", Lines=FGES_Data)
     
 @application.route("/FCI", methods=['GET', 'POST'])
 def FCI_Page():
     if request.method == 'GET':
         fileName = request.args.get('name', None)
-
         return render_template("FCI.html", name=fileName)
 
     if request.method == 'POST':
         fileName = request.form['fileName']
         delimiter = request.form['delim']
-
         df = pd.read_csv(fileName, sep=delimiter)
-
         fciObject = FCI.FCI(df)
-        # os.remove(fileName)   
         return render_template("FCI_Output.html")
 
 @application.route("/GES", methods=['GET', 'POST'])
 def GES_Page():
     if request.method == 'GET':
         fileName = request.args.get('name', None)
-
         return render_template("GES.html", name=fileName)
 
     if request.method == 'POST':
         fileName = request.form['fileName']
         delimiter = request.form['delim']
-
         df = pd.read_csv(fileName, sep=delimiter)
-
         gesObject = GES.GES(df, "static/images/GES_Output.png")
-        # os.remove(fileName)
         return render_template("GES_Output.html")
 
 if __name__ == "__main__":
     application.run(debug=True)
+
+
+# TODO
+# Add error catching for wrong delimiters
+# Add comments to code for better readability
